13c2414ce83b70573dcadd398be6cd77ef33b6ab
moh-ashique <mohammedashique942001@gmail.com>
2025-02-27 20:21:05 +0530

    state updation

--- snd/src/axios.js
+++ snd/src/axios.js
@@ -5,23 +5,6 @@ import { baseUrl } from './constants/constant';
 let isRefreshing = false;
 let failedRequestsQueue = [];
 
-// Helper function to get CSRF token from cookies
-const getCSRFToken = () => {
-    const name = 'csrftoken';
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-};
-
 const instance = axios.create({
     baseURL: baseUrl,
     withCredentials: true,
@@ -34,15 +17,6 @@ const instance = axios.create({
 instance.interceptors.request.use(
     (config) => {
         console.log('Request URL:', config.url);
-        
-        // Add CSRF token for unsafe methods
-        if (['post', 'put', 'patch', 'delete'].includes(config.method)) {
-            const csrfToken = getCSRFToken();
-            if (csrfToken) {
-                config.headers['X-CSRFToken'] = csrfToken;
-            }
-        }
-        
         console.log('Request Headers:', config.headers);
         return config;
     },
@@ -56,13 +30,8 @@ instance.interceptors.response.use(
     },
     async (error) => {
         const originalRequest = error.config;
-        
-        // If the error has no response, just reject
-        if (!error.response) {
-            return Promise.reject(error);
-        }
 
-        if (error.response.status === 401 && !originalRequest._retry) {
+        if (error.response?.status === 401 && !originalRequest._retry) {
             // Prevent infinite loops
             if (isRefreshing) {
                 return new Promise((resolve, reject) => {
@@ -80,16 +49,6 @@ instance.interceptors.response.use(
 
             try {
                 const newAccessToken = await refreshToken();
-                
-                // After successful token refresh, get a new CSRF token
-                // For methods that require CSRF protection
-                if (['post', 'put', 'patch', 'delete'].includes(originalRequest.method)) {
-                    const csrfToken = getCSRFToken();
-                    if (csrfToken) {
-                        originalRequest.headers['X-CSRFToken'] = csrfToken;
-                    }
-                }
-                
                 failedRequestsQueue.forEach((req) => req.resolve(newAccessToken));
                 failedRequestsQueue = [];
                 originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
